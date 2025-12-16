2156589175577efb999ffd3bc54d525f3b22f062
mugni1 <asepabdulmugni22@gmail.com>
2025-12-16 12:48:49 +0700

    refactor: change cors

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,15 @@ import PaymentRoute from "./routes/payment.route.js"
 // initialization
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: [
+        "https://port1.mugni.my.id",
+        "https://fe-midtrans-vue.vercel.app"
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
 
 // routes
 app.get("/", (req: Request, res: Response) => response({ res, message: "Welcome Bro" }))
