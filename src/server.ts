7b9239fb70f576150cd7c37f0f298a8f3ab54995
mugni1 <asepabdulmugni22@gmail.com>
2025-12-16 16:43:08 +0700

    feat: add vite port

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: [
+        "http://localhost:5173",
         "https://app.port1.mugni.my.id",
         "https://fe-midtrans-vue.vercel.app"
     ],
