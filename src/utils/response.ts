a2222b5486409eea591f2d0a042fae25f5daf4cd
mugni1 <asepabdulmugni22@gmail.com>
2025-12-17 19:08:34 +0700

    refactor: improve response

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,5 +1,5 @@
 import { Response } from "express"
 
 export const response = ({ res, status = 200, message = "Message Response", data = null, meta = null, errors = null }: { res: Response, status?: number, message?: string, data?: any, meta?: any, errors?: any }) => {
-    return res.status(status).json({ message, data, meta, errors })
+    return res.status(status).json({ status, message, data, meta, errors })
 }
